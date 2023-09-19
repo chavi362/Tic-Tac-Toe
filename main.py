a7cddb3504ae0b0ee882a20556f7b2f1b43e09aa
chavi362 <140444217+chavi362@users.noreply.github.com>
2023-09-19 23:54:19 +0300

    solving-bugs

--- main.py
+++ main.py
@@ -2,43 +2,47 @@ import pygame
 import sys
 import numpy as np
 
-
 pygame.init()
 WIDTH = 600
-HEIGHT = 600
-LINE_WIDTH = 15
+HEIGHT = WIDTH
 BOARD_ROWS = 3
 BOARD_COLS = 3
+SQURE_SIZE = WIDTH // BOARD_COLS
+LINE_WIDTH = 15
 RED = (255, 0, 0)
 LINE_COLOR = (23, 185, 135)
 tic_screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Let's Play Tic Tac Toe")
 Background_color = (28, 170, 156)
 tic_screen.fill(Background_color)
-CIRCLE_RADIUS =60
-CIRCLE_WIDTH =15
-CIRCLE_COLOR =(239, 234, 200)
-CROSS_WIDTH = 25
-SPACE = 45
-CROSS_COLOR=(66,66,66)
+CIRCLE_RADIUS = SQURE_SIZE // 3
+CIRCLE_WIDTH = SQURE_SIZE //13
+CIRCLE_COLOR = (239, 234, 200)
+CROSS_WIDTH = SQURE_SIZE //8
+SPACE = SQURE_SIZE // 4
+CROSS_COLOR = (66, 66, 66)
 board = np.zeros((BOARD_ROWS, BOARD_COLS))
 
 
 def draw_lines():
-    pygame.draw.line(tic_screen, LINE_COLOR, (0, 200), (600, 200), LINE_WIDTH)
-    pygame.draw.line(tic_screen, LINE_COLOR, (0, 400), (600, 400), LINE_WIDTH)
-    pygame.draw.line(tic_screen, LINE_COLOR, (200, 0), (200, 600), LINE_WIDTH)
-    pygame.draw.line(tic_screen, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
+    pygame.draw.line(tic_screen, LINE_COLOR, (0, SQURE_SIZE), (WIDTH, SQURE_SIZE), LINE_WIDTH)
+    pygame.draw.line(tic_screen, LINE_COLOR, (0, SQURE_SIZE * 2), (WIDTH, SQURE_SIZE * 2), LINE_WIDTH)
+    pygame.draw.line(tic_screen, LINE_COLOR, (0, SQURE_SIZE), (HEIGHT, SQURE_SIZE), LINE_WIDTH)
+    pygame.draw.line(tic_screen, LINE_COLOR, (SQURE_SIZE, 0), (SQURE_SIZE, HEIGHT), LINE_WIDTH)
+    pygame.draw.line(tic_screen, LINE_COLOR, (SQURE_SIZE * 2, 0), (SQURE_SIZE * 2, HEIGHT), LINE_WIDTH)
+
 
 def draw_figuers():
     for row in range(BOARD_ROWS):
         for col in range(BOARD_COLS):
             if board[row][col] == 1:
-                pygame.draw.circle(tic_screen, CIRCLE_COLOR, (int( col * 200 + 200/2), int (row * 200 + 100)), CIRCLE_RADIUS, CIRCLE_WIDTH)
+                pygame.draw.circle(tic_screen, CIRCLE_COLOR, (int(col * SQURE_SIZE + SQURE_SIZE / 2), int(row * SQURE_SIZE + SQURE_SIZE/2)),
+                                   CIRCLE_RADIUS, CIRCLE_WIDTH)
             elif board[row][col] == 2:
-                pygame.draw.line(tic_screen, CROSS_COLOR, (col*200 +SPACE, row*200+200-SPACE),(col*200 +200-SPACE, row* 200+SPACE),CROSS_WIDTH)
-                pygame.draw.line(tic_screen, CROSS_COLOR, (col*200 + SPACE, row*200+SPACE),(col*200 +200- SPACE, row* 200+200- SPACE),CROSS_WIDTH)
-
+                pygame.draw.line(tic_screen, CROSS_COLOR, (col * SQURE_SIZE + SPACE, row * SQURE_SIZE + SQURE_SIZE - SPACE),
+                                 (col * SQURE_SIZE + SQURE_SIZE - SPACE, row * SQURE_SIZE + SPACE), CROSS_WIDTH)
+                pygame.draw.line(tic_screen, CROSS_COLOR, (col * SQURE_SIZE + SPACE, row * SQURE_SIZE + SPACE),
+                                 (col * SQURE_SIZE + SQURE_SIZE - SPACE, row * SQURE_SIZE + SQURE_SIZE - SPACE), CROSS_WIDTH)
 
 
 def mark_square(row, col, player):
@@ -56,9 +60,11 @@ def is_board_full():
                 return False
     return True
 
+
 draw_lines()
 player = 1
-gameOver =False
+gameOver = False
+
 
 def check_win(player):
     for col in range(BOARD_COLS):
@@ -79,24 +85,24 @@ def check_win(player):
             return True
 
 
-
-
 def draw_verticl_winning_line(col, player):
-    posX = col*200 +100
+    posX = col * SQURE_SIZE + SQURE_SIZE // 2
     if player == 1:
         color = CIRCLE_COLOR
     else:
         color = CROSS_COLOR
 
-    pygame.draw.line(tic_screen, color, (posX,15), (posX, HEIGHT-15),20)
+    pygame.draw.line(tic_screen, color, (posX, 15), (posX, HEIGHT - 15), 20)
+
 
 def draw_horizal_winning_line(row, player):
-    posY = row*200 +100
+    posY = row * SQURE_SIZE + SQURE_SIZE //2
     if player == 1:
         color = CIRCLE_COLOR
     else:
         color = CROSS_COLOR
-    pygame.draw.line(tic_screen, color, (15, posY),  (WIDTH -15,posY),15)
+    pygame.draw.line(tic_screen, color, (15, posY), (WIDTH - 15, posY), 15)
+
 
 def draw_asc_diagonal(player):
     if player == 1:
@@ -104,37 +110,43 @@ def draw_asc_diagonal(player):
     else:
         color = CROSS_COLOR
 
-    pygame.draw.line( tic_screen ,color, (15,HEIGHT-15),( WIDTH-15,15),15)
+    pygame.draw.line(tic_screen, color, (15, HEIGHT - 15), (WIDTH - 15, 15), 15)
+
+
 def draw_desc_diagnoal(player):
     if player == 1:
         color = CIRCLE_COLOR
     else:
         color = CROSS_COLOR
-    pygame.draw.line(tic_screen, color, (15,15), (WIDTH-15, HEIGHT-15),20)
-
+    pygame.draw.line(tic_screen, color, (15, 15), (WIDTH - 15, HEIGHT - 15), 20)
 
 
 def restart():
     tic_screen.fill(Background_color)
     draw_lines()
-    player=1
+    player = 1
     for row in range(BOARD_ROWS):
         for col in range(BOARD_COLS):
-            board[row][col]
+            board[row][col] = 0
+    draw_figuers()
+
 
 # main loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                restart()
 
         if event.type == pygame.MOUSEBUTTONDOWN and not gameOver:
 
             mouseX = event.pos[0]
             mouseY = event.pos[1]
 
-            clicked_row = int(mouseY // 200)
-            clicked_col = int(mouseX // 200)
+            clicked_row = int(mouseY // SQURE_SIZE)
+            clicked_col = int(mouseX // SQURE_SIZE)
 
             if available_square(clicked_row, clicked_col):
                 mark_square(clicked_row, clicked_col, player)
@@ -145,8 +157,5 @@ while True:
                     player = 1
                 draw_figuers()
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
-                    restart()
 
     pygame.display.update()
